718cd2ac2e4138c3510119105078ff6f6c422440
kastes <kastes68@mail.ru>
2025-10-05 21:41:34 +0300

    5.4 Введение в Playwright. Практика. Добавлен playwright_registration.py

---
+++ playwright_registration.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
+    )
+
+    email_input = page.get_by_test_id("registration-form-email-input").locator(
+        "//div//input"
+    )
+    email_input.fill("user.name@gmail.com")
+
+    username_input = page.get_by_test_id("registration-form-username-input").locator(
+        "//div//input"
+    )
+    username_input.fill("username")
+
+    password_input = page.get_by_test_id("registration-form-password-input").locator(
+        "//div//input"
+    )
+    password_input.fill("password")
+
+    registration_button = page.get_by_test_id("registration-page-registration-button")
+    registration_button.click()
+
+    dashboard_toolbar_title_text = page.get_by_test_id("dashboard-toolbar-title-text")
+    expect(dashboard_toolbar_title_text).to_be_visible()
+    expect(dashboard_toolbar_title_text).to_have_text("Dashboard")
+
+    page.wait_for_timeout(3000)  # для демонстрации результата
