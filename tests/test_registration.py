55c21057a7bf517450c07516238edf9a04f88963
kastes <kastes68@mail.ru>
2025-10-08 11:50:24 +0300

    6.3 Маркировки Pytest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,6 +1,10 @@
-from playwright.sync_api import sync_playwright, expect
+import pytest
+
+from playwright.sync_api import expect, sync_playwright
 
 
+@pytest.mark.regression
+@pytest.mark.registration
 def test_succesfull_registration():
 
     with sync_playwright() as playwright:
