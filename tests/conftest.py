5cad3eefc1b11cda9d22d0b17f6454e99922410f
kastes <kastes68@mail.ru>
2025-10-12 11:08:03 +0300

    6.5 Фикстуры Pytest. Установка плагина pytest-playwright

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
 
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import Playwright, Page
 from typing import Generator
 
 
 @pytest.fixture
-def chromium_page() -> Generator[Page]:
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        yield browser.new_page()
+def chromium_page(playwright: Playwright) -> Generator[Page]:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
