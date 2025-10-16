6f3429b9aec00dfa0600de89ecb1628fcfc4c396
kastes <kastes68@mail.ru>
2025-10-16 23:25:32 +0300

    7.4 Практикуемся в реализации PageComponent. CourseViewComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -27,23 +27,31 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
 
     create_course_page.check_visible_create_course_title()  # 2
     create_course_page.check_disabled_create_course_button()  # 3
+
     create_course_page.check_visible_image_preview_empty_view()  # 4
+
     create_course_page.check_visible_image_upload_view(is_image_uploaded=False)  # 5
     create_course_page.check_visible_create_course_form(
         title="", estimated_time="", description="", max_score="0", min_score="0"
     )  # 6
     create_course_page.check_visible_exercises_title()  # 7
     create_course_page.check_visible_create_exercise_button()  # 8
+
     create_course_page.check_visible_exercises_empty_view()  # 9
+
     create_course_page.upload_preview_image("./testdata/files/playwright-logo.png")  # 10
+
     create_course_page.check_visible_image_upload_view(is_image_uploaded=True)  # 11
+
     create_course_page.check_visible_preview_image()
+
     create_course_page.fill_create_course_form(
         title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10"
     )  # 12
     create_course_page.click_create_course_button()  # 13
+
     courses_list_page.check_visible_courses_title()  # 14
     courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
+    courses_list_page.course_view.check_visible(
         index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
     )  # 15
