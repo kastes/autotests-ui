f8c7f04d71d72754c62861ed2d8ccea8475753bd
kastes <kastes68@mail.ru>
2025-10-19 10:55:47 +0300

    7.4 Практикуемся в реализации PageComponent. CreateCourseExercisesToolbarViewComponent

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -1,10 +1,11 @@
-from playwright.sync_api import expect, Page
+from playwright.sync_api import Page
 
 from components.navigation.navbar_component import NavbarComponent
 from components.navigation.sidebar_component import SideBarComponent
 from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
 from components.views.image_upload_widget_component import EmptyViewComponent, ImageUploadWidgetComponent
 from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
+from components.courses.create_course_exercises_toolbar_view_component import CreateCourseExercisesToolbarViewComponent
 from components.courses.create_course_form_component import CreateCourseFormComponent
 from pages.base_page import BasePage
 
@@ -15,25 +16,12 @@ class CreateCoursePage(BasePage):
 
         self.navbar = NavbarComponent(page)
         self.sidebar = SideBarComponent(page)
-        self.toolbar_view = CreateCourseToolbarViewComponent(page)
+        self.create_course_toolbar_view = CreateCourseToolbarViewComponent(page)
         self.image_upload_widget = ImageUploadWidgetComponent(page, identifier="create-course-preview")
         self.create_course_form = CreateCourseFormComponent(page)
+        self.create_exercises_toolbar_view = CreateCourseExercisesToolbarViewComponent(page)
         self.exercises_empty_view = EmptyViewComponent(page, identifier="create-course-exercises")
         self.create_course_exercise_form = CreateCourseExerciseFormComponent(page)
-
-        # блок добавления упражнений
-        self.exercises_title = page.get_by_test_id("create-course-exercises-box-toolbar-title-text")
-        self.create_exercise_button = page.get_by_test_id("create-course-exercises-box-toolbar-create-exercise-button")
-
-    def check_visible_exercises_title(self):
-        expect(self.exercises_title).to_be_visible()
-        expect(self.exercises_title).to_have_text("Exercises")
-
-    def check_visible_create_exercise_button(self):
-        expect(self.create_exercise_button).to_be_visible()
-
-    def click_create_exercise_button(self):
-        self.create_exercise_button.click()
 
     def check_visible_exercises_empty_view(self):
         self.exercises_empty_view.check_visible(
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -18,24 +18,23 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
 @pytest.mark.regression
 @pytest.mark.courses
 def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
-    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")  # 1
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
     courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
 
-    create_course_page.toolbar_view.check_visible()  # 2
+    create_course_page.create_course_toolbar_view.check_visible()
 
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
 
     create_course_page.create_course_form.check_visible(
         title="", estimated_time="", description="", max_score="0", min_score="0"
     )  # 6
-    create_course_page.check_visible_exercises_title()  # 7
-    create_course_page.check_visible_create_exercise_button()  # 8
+    create_course_page.create_exercises_toolbar_view.check_visible()
 
-    create_course_page.check_visible_exercises_empty_view()  # 9
+    create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/playwright-logo.png")  # 10
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/playwright-logo.png")
 
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
 
@@ -43,10 +42,10 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
         title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10"
     )  # 12
 
-    create_course_page.toolbar_view.check_visible(is_create_course_disabled=False)  # 2
-    create_course_page.toolbar_view.click_create_course_button()  # 13
+    create_course_page.create_course_toolbar_view.check_visible(is_create_course_disabled=False)
+    create_course_page.create_course_toolbar_view.click_create_course_button()
 
     courses_list_page.toolbar_view.check_visible()
     courses_list_page.course_view.check_visible(
         index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
-    )  # 15
+    )
