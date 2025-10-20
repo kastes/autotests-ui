8757b7504911b151306912f7482c4b46eb7b396e
kastes <kastes68@mail.ru>
2025-10-20 20:02:56 +0300

    7.6 Практическое применение PageFactory в автотестах. Работа с n-м элементом в локаторе. CourseViewComponent

--- components/courses/course_view_component.py
+++ components/courses/course_view_component.py
@@ -1,7 +1,9 @@
-from playwright.sync_api import expect, Page
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
 from components.courses.course_view_menu_component import CourseViewMenuComponent
+from elements.image import Image
+from elements.text import Text
 
 
 class CourseViewComponent(BaseComponent):
@@ -10,25 +12,24 @@ class CourseViewComponent(BaseComponent):
 
         self.menu = CourseViewMenuComponent(page)
 
-        self.title = page.get_by_test_id("course-widget-title-text")
-        self.image = page.get_by_test_id("course-preview-image")
-        self.max_score_text = page.get_by_test_id("course-max-score-info-row-view-text")
-        self.min_score_text = page.get_by_test_id("course-min-score-info-row-view-text")
-        self.estimated_time_text = page.get_by_test_id("course-estimated-time-info-row-view-text")
+        self.title = Text(page, "course-widget-title-text", "Title")
+        self.image = Image(page, "course-preview-image", "Preview")
+        self.max_score_text = Text(page, "course-max-score-info-row-view-text", "Max score")
+        self.min_score_text = Text(page, "course-min-score-info-row-view-text", "Min score")
+        self.estimated_time_text = Text(page, "course-estimated-time-info-row-view-text", "Estimated time")
 
     def check_visible(self, index: int, title: str, max_score: str, min_score: str, estimated_time: str):
         self.menu.check_visible(index)
 
-        expect(self.title.nth(index)).to_be_visible()
-        expect(self.title.nth(index)).to_have_text(title)
+        self.title.check_visible(index)
+        self.title.check_have_text(title, index)
 
-        expect(self.image.nth(index)).to_be_visible()
+        self.image.check_visible(index)
 
-        expect(self.max_score_text.nth(index)).to_be_visible()
-        expect(self.max_score_text.nth(index)).to_have_text(f"Max score: {max_score}")
+        self.max_score_text.check_visible(index)
+        self.max_score_text.check_have_text(f"Max score: {max_score}", index)
+        self.min_score_text.check_visible(index)
+        self.min_score_text.check_have_text(f"Min score: {min_score}", index)
 
-        expect(self.min_score_text.nth(index)).to_be_visible()
-        expect(self.min_score_text.nth(index)).to_have_text(f"Min score: {min_score}")
-
-        expect(self.estimated_time_text.nth(index)).to_be_visible()
-        expect(self.estimated_time_text.nth(index)).to_have_text(f"Estimated time: {estimated_time}")
+        self.estimated_time_text.check_visible(index)
+        self.estimated_time_text.check_have_text(f"Estimated time: {estimated_time}", index)
--- elements/base_element.py
+++ elements/base_element.py
@@ -7,22 +7,22 @@ class BaseElement:
         self.locator: str = locator
         self.name: str = name
 
-    def get_locator(self, **kwargs) -> Locator:
-        locator: str = self.locator.format(**kwargs)
-        return self.page.get_by_test_id(locator)
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
+        locator: str = self.locator.format(nth, **kwargs)
+        return self.page.get_by_test_id(locator).nth(nth)
 
-    def check_visible(self, **kwargs) -> None:
-        locator: Locator = self.get_locator(**kwargs)
+    def check_visible(self, nth: int = 0, **kwargs) -> None:
+        locator: Locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs) -> None:
-        locator: Locator = self.get_locator(**kwargs)
+    def check_have_text(self, text: str, nth: int = 0, **kwargs) -> None:
+        locator: Locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_text(text)
 
-    def click(self, **kwargs) -> None:
-        locator: Locator = self.get_locator(**kwargs)
+    def click(self, nth: int = 0, **kwargs) -> None:
+        locator: Locator = self.get_locator(nth, **kwargs)
         locator.click()
 
-    def hover(self, **kwargs) -> None:
-        locator: Locator = self.get_locator(**kwargs)
+    def hover(self, nth: int = 0, **kwargs) -> None:
+        locator: Locator = self.get_locator(nth, **kwargs)
         locator.hover()
