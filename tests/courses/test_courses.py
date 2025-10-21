769b599b3c025f779a13030c0ef6ac227f43712d
kastes <kastes68@mail.ru>
2025-10-21 22:51:21 +0300

    7.7  Наращивание тестовой базы и рефакторинг. test_courses/test_edit_course

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -47,3 +47,37 @@ class TestCourses:
         courses_list_page.course_view.check_visible(
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
         )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/python-logo.jpeg")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title="Python", estimated_time="6 months", description="Python language", max_score="1000", min_score="900"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Python", max_score="1000", min_score="900", estimated_time="6 months"
+        )
+        courses_list_page.course_view.menu.check_visible(index=0)
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/playwright-logo.png")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="4 months",
+            description="Playwright framework",
+            max_score="100",
+            min_score="90",
+        )
+
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Playwright", max_score="100", min_score="90", estimated_time="4 months"
+        )
