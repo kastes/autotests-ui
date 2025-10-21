a17cb6d8f2e6f626fa15bb8fb98960d07bf5ee62
kastes <kastes68@mail.ru>
2025-10-21 21:03:24 +0300

    7.7  Наращивание тестовой базы и рефакторинг. test_succesfull_authorization

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,11 +1,34 @@
 import pytest
 
 from pages.authentication.login_page import LoginPage
+from pages.authentication.registration_page import RegistrationPage
+from pages.dashboard.dashboard_page import DashboardPage
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
 class TestAuthorization:
+    def test_succesfull_authorization(
+        self, registration_page: RegistrationPage, login_page: LoginPage, dashboard_page: DashboardPage
+    ):
+        registration_page.visit(
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
+        )
+        registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+        registration_page.click_registration_button()
+
+        dashboard_page.dashboard_toolbar_view.check_visible()
+        dashboard_page.navbar.check_visible(username="username")
+        dashboard_page.sidebar.check_visible()
+        dashboard_page.sidebar.click_logout()
+
+        login_page.login_form.fill(email="user.name@gmail.com", password="password")
+        login_page.click_login_button()
+
+        dashboard_page.dashboard_toolbar_view.check_visible()
+        dashboard_page.navbar.check_visible(username="username")
+        dashboard_page.sidebar.check_visible()
+
     @pytest.mark.parametrize(
         "email,password",
         [
