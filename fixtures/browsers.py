c4916f827ada442fa8708e2a1cd9f5d167d434a3
kastes <kastes68@mail.ru>
2025-10-28 09:13:54 +0300

    8.5 Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -3,23 +3,11 @@ import pytest
 
 from typing import Generator
 
+from allure_commons.types import AttachmentType
 from playwright.sync_api import Playwright, Page
 from _pytest.fixtures import SubRequest
 
 from pages.authentication.registration_page import RegistrationPage
-
-
-@pytest.fixture
-def chromium_page(playwright: Playwright, request: SubRequest) -> Generator[Page]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f"./tracing/{request.node.name}.zip")
-    browser.close()
-    allure.attach.file(f"./tracing/{request.node.name}.zip", name="trace", extension="zip")
 
 
 @pytest.fixture(scope="session")
@@ -39,15 +27,40 @@ def initialize_browser_state(playwright: Playwright) -> None:
 
 
 @pytest.fixture
+def chromium_page(playwright: Playwright, request: SubRequest) -> Generator[Page]:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(record_video_dir="./videos")
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
+
+    yield page
+
+    context.tracing.stop(path=f"./tracing/{request.node.name}.zip")
+    browser.close()
+
+    allure.attach.file(f"./tracing/{request.node.name}.zip", name="trace", extension="zip")
+    assert (
+        page.video is not None
+    )  # без этой проверки mypy выдаёт ошибку, т.к. тип свойства page.video - Union[Video, None]
+    allure.attach.file(page.video.path(), name="video", attachment_type=AttachmentType.WEBM)
+
+
+@pytest.fixture
 def chromium_page_with_state(
     initialize_browser_state: None, playwright: Playwright, request: SubRequest
 ) -> Generator[Page]:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
+    context = browser.new_context(storage_state="browser-state.json", record_video_dir="./videos")
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
 
-    yield context.new_page()
+    yield page
 
     context.tracing.stop(path=f"./tracing/{request.node.name}.zip")
     browser.close()
+
     allure.attach.file(f"./tracing/{request.node.name}.zip", name="trace", extension="zip")
+    if (
+        page.video is not None
+    ):  # без этой проверки mypy выдаёт ошибку, т.к. тип свойства page.video - Union[Video, None]
+        allure.attach.file(page.video.path(), name="video", attachment_type=AttachmentType.WEBM)
