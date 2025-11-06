55f9b41110203a847e10262dec92eefa5f8e5b75
Константин <kastes68@mail.ru>
2025-11-06 23:06:03 +0300

    Очистка ветки gh-pages от ненужных файлов и каталогов

--- tools/allure/stories.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class AllureStory(str, Enum):
-    COURSES = "Courses"
-    DASHBOARD = "Dashboard"
-    AUTHORIZATION = "Authorization"
-    REGISTRATION = "Registration"
