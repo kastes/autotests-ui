d9c88839367e6841934b49e788784867156e1d64
kastes <kastes68@mail.ru>
2025-11-09 13:58:21 +0300

    10.4 Запуск автотестов на CI. Рефакторинг

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,7 +2,6 @@ import allure
 
 from typing import Generator
 
-from allure_commons.types import AttachmentType
 from playwright.sync_api import Playwright, Page
 
 from config import settings, Browser
@@ -29,7 +28,7 @@ def initiliaze_playwright_page(
         page.video is not None
     ):  # без этой проверки mypy выдаёт ошибку, т.к. тип свойства page.video - Union[Video, None]. Ещё можно
         # использовать assert page.video is not None или assert isinstance(page.video, Video)
-        allure.attach.file(page.video.path(), name="video", attachment_type=AttachmentType.WEBM)
+        allure.attach.file(page.video.path(), name="video", attachment_type=allure.attachment_type.WEBM)
 
     page.close()
     context.close()
